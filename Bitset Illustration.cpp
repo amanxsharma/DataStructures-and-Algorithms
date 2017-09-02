2d454a8440c61e5d26cbb00e32b028c34cfaf1fe
amanxsharma <aman.sharma728@gmail.com>
2017-09-02 17:06:28 -0400

    added some comments

--- Bitset Illustration.cpp
+++ Bitset Illustration.cpp
@@ -47,5 +47,9 @@ int main() {
 	  std::cout << (foo|bar) << '\n';        // 0111
 	  std::cout << (foo^bar) << '\n';        // 0101
 
+//       Also to toggle or flip bits
+//       std::cout << foo.flip(2) << '\n';     // 0101
+//       std::cout << foo.flip() << '\n';      // 1010
+
 	return 0;
 }
